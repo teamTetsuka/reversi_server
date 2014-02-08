f2ba29bd1ac55a4372896c14f115682db2db5c6c
YukiTETSUKA <yuki@tedzukayuuki-no-MacBook-Air.local>
2014-02-08 16:19:14 +0900

    SELECT文

--- app_start.php
+++ app_start.php
@@ -1,47 +1,35 @@
 <?php
-//    // 環境変数参照
-//    $vcap_services = getenv('VCAP_SERVICES');
-//    if($vcap_services === false){
-//          exit();
-//    }
-//
-//    // 環境変数からDB設定を取得
-//    $vcap_services_json = json_decode($vcap_services);
-//    //var_dump($vcap_services_json);
-//
-//    // DB接続
-//    $pdo = null;
-//    try {
-//        $pdo = new PDO(
-//            "mysql:host={$db->host};port={$db->port};dbname={$db->name}",
-//            $db->username,
-//            $db->password,
-//            array(
-//                PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET utf8"
-//            )
-//        );
-//    } catch (Exception $e) {
-//        //print_r( $e );
-//        exit();
-//    }
-
-    $services_json = getenv('VCAP_SERVICES');
-    $services = json_decode($services_json, true);
-    $config = null;
-    foreach ($services as $name => $service) {
-        if (0 === stripos($name, 'mysql')) {
-            $config = $service[0]['credentials'];
-            break;
-        }
+    // 環境変数参照
+    $vcap_services = getenv('VCAP_SERVICES');
+    if($vcap_services === false){
+          exit();
     }
-    is_null($config) && die('MySQL service information not found.');
 
-    var_dump($config);
+    // 環境変数からDB設定を取得
+    $vcap_services_json = json_decode($vcap_services);
+    //var_dump($vcap_services_json);
 
-//    $db_hostname = $config["hostname"];
-//    $db_hostport = $config["port"];
-//    $db_username = $config["user"];
-//    $db_password = $config["password"];
+    // DB接続
+    $pdo = null;
+    try {
+        $pdo = new PDO(
+            "mysql:host={$db->host};port={$db->port};dbname={$db->name}",
+            $db->username,
+            $db->password,
+            array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET utf8"
+            )
+        );
+    } catch (Exception $e) {
+        //print_r( $e );
+        exit();
+    }
+
+    $sql = "SELECT * FROM fixtures WHERE quantity=1";
+    $params = array();
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($params);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <?php
