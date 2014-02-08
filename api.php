2742dd65af473716ab11c4f2a481a79e6b2267e2
YukiTETSUKA <yuki@tedzukayuuki-no-MacBook-Air.local>
2014-02-08 16:47:48 +0900

    path確認

--- api.php
+++ api.php
@@ -1,44 +1,36 @@
 <?php
-    var_dump($_POST);
-    var_dump($_SERVER);
+$params = explode("/", $_SERVER['PATH_INFO']);
+var_dump($params);
 
-    // 環境変数参照
-    $vcap_services = getenv('VCAP_SERVICES');
-    if($vcap_services === false){
-          exit();
-    }
+// 環境変数参照
+$vcap_services = getenv('VCAP_SERVICES');
+if($vcap_services === false){
+    exit();
+}
 
-    // 環境変数からDB設定を取得
-    $vcap_services_json = json_decode($vcap_services);
-    //var_dump($vcap_services_json);
+// 環境変数からDB設定を取得
+$vcap_services_json = json_decode($vcap_services);
 
-    // DB接続
-    $pdo = null;
-    try {
-        $pdo = new PDO(
-            "mysql:host={$db->host};port={$db->port};dbname={$db->name}",
-            $db->username,
-            $db->password,
-            array(
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET utf8"
-            )
-        );
-    } catch (Exception $e) {
-        //print_r( $e );
-        exit();
-    }
+// DB接続
+$pdo = null;
+try {
+    $pdo = new PDO(
+        "mysql:host={$db->host};port={$db->port};dbname={$db->name}",
+        $db->username,
+        $db->password,
+        array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET utf8"
+        )
+    );
+} catch (Exception $e) {
+    //print_r( $e );
+    exit();
+}
 
-    $sql = "SELECT * FROM fixtures WHERE quantity=1";
-    $params = array();
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute($params);
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$sql = "SELECT * FROM fixtures WHERE quantity=1";
+$params = array();
+$stmt = $pdo->prepare($sql);
+$stmt->execute($params);
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    var_dump($results);
-?>
-
-<?php
-    // json形式で出力
-    //$json = array('session_id' => $session_id, 'player_id' => $player_id);
-
-    //echo json_encode($json);
+var_dump($results);
